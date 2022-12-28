bce967bdf1e96dc9034964ff2ab1e4a2f22fae10
kevinmoral.es <hello@kevinmoral.es>
2022-12-28 11:49:21 -0600

    first

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'theyr-in-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'theyr-in';
+}
